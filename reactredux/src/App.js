695c105218dbbad304c83f93842d2bf791669049
Lucas362 <lucasvitorifg@gmail.com>
2020-08-07 23:50:20 -0300

    Adding redux to sidebar

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,48 +1,20 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 
 import Video from './components/Video';
 import Sidebar from './components/Sidebar';
 
+import store from './store';
+
 class App extends Component {
-    state = {
-        activeLesson: {},
-        modules: [
-            {
-                id: 1,
-                title: 'Iniciando com React',
-                lessons: [
-                    {
-                        id: 1,
-                        title: 'Primeira aula',
-                    },
-                    {
-                        id: 2,
-                        title: 'Segunda aula',
-                    },
-                ]
-            },
-            {
-                id: 2,
-                title: 'Aprendendo Redux',
-                lessons: [
-                    {
-                        id: 3,
-                        title: 'Terceira aula',
-                    },
-                    {
-                        id: 4,
-                        title: 'Quarta aula',
-                    },
-                ]
-            },
-        ]
-    };
 
     render() {
         return (
             <div className="App">
-                <Video activeLesson={this.state.activeLesson} />
-                <Sidebar modules={this.state.modules} activeLesson={this.state.activeLesson} />
+                <Provider store={store}>
+                    <Video />
+                    <Sidebar />
+                </Provider>
             </div>
         );
     }
